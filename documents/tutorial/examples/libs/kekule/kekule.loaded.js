fb9b4863dc530b3b81a1ff12b2b110083ef4126a
partridgejiang <partridge@cpu.edu.cn>
2016-05-08 21:44:00 +0800

    Remove tutorial for error

--- documents/tutorial/examples/libs/kekule/kekule.loaded.js
+++
@@ -1,8 +0,0 @@
-/**
- * A special JS file that need to be compressed into kekule.min.js or
- * any other compressed files that load directly in HTML document (rather
- * than using kekule.js loader).
- */
-
-if (this.Kekule)
-	Kekule._loaded();
