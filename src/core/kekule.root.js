2a0ae76c3dcf974a9be0c4ed4cbae5d6d7e41ad6
partridgejiang <partridge@cpu.edu.cn>
2016-05-08 20:53:02 +0800

    Update release files

--- src/core/kekule.root.js
+++ src/core/kekule.root.js
@@ -9,7 +9,7 @@
 var Kekule = {
 	LIBNAME: 'Kekule.js',
 	LIBNAME_CORE: 'Kekule',
-	VERSION: '0.6.0',
+	VERSION: '0.7.0',
 	/**
 	 * A flag that indicate whether all essential Kekule modules are loaded into document.
 	 * @ignore
