4792e5690c8b34779525736f7bf7e9cd082e8670
partridgejiang <partridge@cpu.edu.cn>
2016-09-11 11:31:55 +0800

    Refine MolComparer server app

--- src/_extras/MoodleExtensions/jsServer/routes/mols.js
+++ src/_extras/MoodleExtensions/jsServer/routes/mols.js
@@ -8,13 +8,27 @@ router.post('/compare', function(req, res, next) {
   var sSource = req.body.sourceMol;
   var sTarget = req.body.targetMol;
   var mimeType = req.body.mimeType || Kekule.IO.MimeType.JSON;
-  var options = JSON.parse(req.body.options || '{}');
-  // parse source and target molecule
-  var srcMol = Kekule.IO.loadMimeData(sSource, mimeType);
-  var targetMol = Kekule.IO.loadMimeData(sTarget, mimeType);
-  var result = srcMol.isSameStructureWith(targetMol, options);
+  var result = {};
+  try
+  {
+    var options = JSON.parse(req.body.options || '{}');
+    // parse source and target molecule
+    var srcMol = Kekule.IO.loadMimeData(sSource, mimeType);
+    var targetMol = Kekule.IO.loadMimeData(sTarget, mimeType);
+    //console.log('Compare molecules', srcMol, targetMol);
+    result.result = srcMol.isSameStructureWith(targetMol, options);
+  }
+  catch(e)
+  {
+    result.error = e.message;
+  }
+  console.log('Return result', result);
   // returns result as JSON
   res.json(result);
+});
+
+router.get('/compare', function(req, res, next) {
+  res.render('index', { title: 'Mol Comparer' });
 });
 
 module.exports = router;
