886d063fe588754907eccbaf8d7ddf531574a3bb
partridgejiang <partridge@cpu.edu.cn>
2017-02-21 15:33:42 +0800

    Rewrite some unit tests for recent adding of ChemObject.compare method

--- unitTests/core/coreClassTest.js
+++ unitTests/core/coreClassTest.js
@@ -7,18 +7,18 @@ describe('Test of some methods of core classes', function(){
 		var o1 = new Kekule.ChemObject();
 		var o2 = o1.clone(true);
 
-		expect(o1.compareTo(1)).toEqual(1);
-		expect(o1.compareTo(false)).toEqual(1);
-		expect(o1.compareTo({a: 1})).toEqual(1);
-		expect(o1.compareTo(new Date())).toEqual(1);
+		expect(o1.compare(1)).toEqual(1);
+		expect(o1.compare(false)).toEqual(1);
+		expect(o1.compare({a: 1})).toEqual(1);
+		expect(o1.compare(new Date())).toEqual(1);
 
-		expect(o1.compareTo(o2)).toEqual(0);
-		expect(o2.compareTo(o1)).toEqual(0);
+		expect(o1.compare(o2)).toEqual(0);
+		expect(o2.compare(o1)).toEqual(0);
 
 		//o2.setId((o2.getId() || '') + 'dummy');
 		o2.setInfoValue('key1', 'value1');
-		expect(o1.compareTo(o2)).toBeLessThan(0);
-		expect(o2.compareTo(o1)).toBeGreaterThan(0);
+		expect(o1.compare(o2)).toBeLessThan(0);
+		expect(o2.compare(o1)).toBeGreaterThan(0);
 	});
 
 	it('Structure compare method test on atom', function(){
@@ -32,29 +32,29 @@ describe('Test of some methods of core classes', function(){
 		var atomC = new Kekule.Atom('a1', 'C');
 		var atomC13 = new Kekule.Atom('a2', 'C', 13);
 		var atomCStereo = new Kekule.Atom('a3', 'C');
-			atomCStereo.setParity(0);
+			atomCStereo.setParity(2);
 		var atomOStereo = new Kekule.Atom('a4', 'O');
-			atomOStereo.setParity(0);
+			atomOStereo.setParity(2);
 
 		// test on atoms
-		expect(atomC.compareTo(atomC13, ops1)).toEqual(0);
-		expect(atomC.compareTo(atomC13, ops2)).toEqual(0);
-		expect(atomC.compareTo(atomC13, ops3)).toEqual(-1);
-		expect(atomC.compareTo(atomC13, ops4)).toEqual(-1);
+		expect(atomC.compare(atomC13, ops1)).toEqual(0);
+		expect(atomC.compare(atomC13, ops2)).toEqual(0);
+		expect(atomC.compare(atomC13, ops3)).toEqual(-1);
+		expect(atomC.compare(atomC13, ops4)).toEqual(-1);
 
-		expect(atomC.compareTo(atomCStereo, ops1)).toEqual(0);
-		expect(atomC.compareTo(atomCStereo, ops2)).toEqual(0);
-		expect(atomC.compareTo(atomCStereo, ops3)).toEqual(0);
-		expect(atomC.compareTo(atomCStereo, ops4)).not.toEqual(0);
+		expect(atomC.compare(atomCStereo, ops1)).toEqual(0);
+		expect(atomC.compare(atomCStereo, ops2)).toEqual(0);
+		expect(atomC.compare(atomCStereo, ops3)).toEqual(0);
+		expect(atomC.compare(atomCStereo, ops4)).not.toEqual(0);
 
-		expect(atomC.compareTo(atomOStereo, ops1)).toEqual(0);
-		expect(atomC.compareTo(atomOStereo, ops2)).toEqual(-1);
-		expect(atomC.compareTo(atomOStereo, ops3)).toEqual(-1);
-		expect(atomC.compareTo(atomOStereo, ops4)).not.toEqual(0);
+		expect(atomC.compare(atomOStereo, ops1)).toEqual(0);
+		expect(atomC.compare(atomOStereo, ops2)).toEqual(-1);
+		expect(atomC.compare(atomOStereo, ops3)).toEqual(-1);
+		expect(atomC.compare(atomOStereo, ops4)).not.toEqual(0);
 
-		expect(atomCStereo.compareTo(atomOStereo, ops1)).toEqual(0);
-		expect(atomCStereo.compareTo(atomOStereo, ops2)).toEqual(-1);
-		expect(atomCStereo.compareTo(atomOStereo, ops5)).toEqual(0);
+		expect(atomCStereo.compare(atomOStereo, ops1)).toEqual(0);
+		expect(atomCStereo.compare(atomOStereo, ops2)).toEqual(-1);
+		expect(atomCStereo.compare(atomOStereo, ops5)).toEqual(0);
 
 		var pseudoAtom1 = new Kekule.Pseudoatom(null, Kekule.PseudoatomType.DUMMY);
 			//pseudoAtom1.setSymbol('CCC1');
@@ -72,14 +72,14 @@ describe('Test of some methods of core classes', function(){
 			pseudoAtom5.setSymbol('CCC2');
 
 		// test on pseudoAtoms
-		expect(pseudoAtom1.compareTo(pseudoAtom1_2, ops4)).toEqual(0);
-		expect(pseudoAtom2.compareTo(pseudoAtom2_2, ops4)).toEqual(0);
-		expect(pseudoAtom1.compareTo(pseudoAtom2, ops1)).toEqual(0);
-		expect(pseudoAtom1.compareTo(pseudoAtom2, ops4)).not.toEqual(0);
-		expect(pseudoAtom1.compareTo(pseudoAtom3, ops4)).not.toEqual(0);
-		expect(pseudoAtom3.compareTo(pseudoAtom2, ops4)).not.toEqual(0);
-		expect(pseudoAtom4.compareTo(pseudoAtom5, ops1)).toEqual(0);
-		expect(pseudoAtom4.compareTo(pseudoAtom5, ops4)).not.toEqual(0);
+		expect(pseudoAtom1.compare(pseudoAtom1_2, ops4)).toEqual(0);
+		expect(pseudoAtom2.compare(pseudoAtom2_2, ops4)).toEqual(0);
+		expect(pseudoAtom1.compare(pseudoAtom2, ops1)).toEqual(0);
+		expect(pseudoAtom1.compare(pseudoAtom2, ops4)).not.toEqual(0);
+		expect(pseudoAtom1.compare(pseudoAtom3, ops4)).not.toEqual(0);
+		expect(pseudoAtom3.compare(pseudoAtom2, ops4)).not.toEqual(0);
+		expect(pseudoAtom4.compare(pseudoAtom5, ops1)).toEqual(0);
+		expect(pseudoAtom4.compare(pseudoAtom5, ops4)).not.toEqual(0);
 
 		var vAtom1 = new Kekule.VariableAtom();
 			vAtom1.setDisallowedIsotopeIds(['O', 'C13', 'P']);
@@ -91,17 +91,17 @@ describe('Test of some methods of core classes', function(){
 			vAtom4.setAllowedIsotopeIds(['O', 'C13', 'P']);
 
 		// test on variable atoms
-		expect(vAtom1.compareTo(vAtom2, ops4)).toEqual(0);
-		expect(vAtom1.compareTo(vAtom3, ops4)).not.toEqual(0);
-		expect(vAtom1.compareTo(vAtom4, ops4)).not.toEqual(0);
-		expect(vAtom3.compareTo(vAtom4, ops4)).toEqual(0);
+		expect(vAtom1.compare(vAtom2, ops4)).toEqual(0);
+		expect(vAtom1.compare(vAtom3, ops4)).not.toEqual(0);
+		expect(vAtom1.compare(vAtom4, ops4)).not.toEqual(0);
+		expect(vAtom3.compare(vAtom4, ops4)).toEqual(0);
 
 
 		// test on different types
-		expect(atomC.compareTo(pseudoAtom1, ops1)).not.toEqual(0);
-		expect(atomC.compareTo(pseudoAtom1, ops2)).not.toEqual(0);
-		expect(atomC.compareTo(vAtom1, ops1)).not.toEqual(0);
-		expect(atomC.compareTo(vAtom1, ops2)).not.toEqual(0);
+		expect(atomC.compare(pseudoAtom1, ops1)).not.toEqual(0);
+		expect(atomC.compare(pseudoAtom1, ops2)).not.toEqual(0);
+		expect(atomC.compare(vAtom1, ops1)).not.toEqual(0);
+		expect(atomC.compare(vAtom1, ops2)).not.toEqual(0);
 	});
 
 	it('Structure compare method test on bond', function(){
@@ -116,13 +116,13 @@ describe('Test of some methods of core classes', function(){
 		var ops2 = {'method': Kekule.ComparisonMethod.CHEM_STRUCTURE, structureLevel: Kekule.StructureComparationLevel.CONSTITUTION};
 		var ops3 = {'method': Kekule.ComparisonMethod.CHEM_STRUCTURE, structureLevel: Kekule.StructureComparationLevel.CONFIGURATION};
 
-		expect(b1.compareTo(b2, ops1)).toEqual(0);
+		expect(b1.compare(b2, ops1)).toEqual(0);
 
-		expect(b2.compareTo(b1, ops1)).toEqual(0);
-		expect(b1.compareTo(b2, ops2)).not.toEqual(0);
-		expect(b2.compareTo(b1, ops2)).not.toEqual(0);
-		expect(b1.compareTo(b2, ops3)).not.toEqual(0);
-		expect(b2.compareTo(b1, ops3)).not.toEqual(0);
+		expect(b2.compare(b1, ops1)).toEqual(0);
+		expect(b1.compare(b2, ops2)).not.toEqual(0);
+		expect(b2.compare(b1, ops2)).not.toEqual(0);
+		expect(b1.compare(b2, ops3)).not.toEqual(0);
+		expect(b2.compare(b1, ops3)).not.toEqual(0);
 
 	});
 });
