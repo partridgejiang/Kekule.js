578fd8ca0260732ec61bd161d200f34dbb6f8846
partridgejiang <partridge@cpu.edu.cn>
2018-04-14 17:10:16 +0800

    Create a new release

--- dist/kekule.js
+++ dist/kekule.js
@@ -378,6 +378,8 @@ var kekuleFiles = {
 			'widgets/chem/editor/kekule.chemEditor.repositories.js',
 			'widgets/chem/editor/kekule.chemEditor.chemSpaceEditors.js',
 			'widgets/chem/editor/kekule.chemEditor.nexus.js',
+			'widgets/chem/editor/kekule.chemEditor.objModifiers.js',
+			'widgets/chem/editor/modifiers/kekule.chemEditor.textModifiers.js',
 			'widgets/chem/editor/kekule.chemEditor.composers.js',
 			'widgets/chem/editor/kekule.chemEditor.actions.js',
 
