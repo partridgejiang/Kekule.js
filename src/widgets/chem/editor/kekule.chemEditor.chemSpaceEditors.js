31dc397f7582d24a669f0f28b96f879bf1807c96
partridgejiang <partridge@cpu.edu.cn>
2018-06-07 22:08:14 +0800

    Adjust the display position of popup text and formula widgets

--- src/widgets/chem/editor/kekule.chemEditor.chemSpaceEditors.js
+++ src/widgets/chem/editor/kekule.chemEditor.chemSpaceEditors.js
@@ -4805,6 +4805,10 @@ Kekule.Editor.FormulaIaController = Class.create(Kekule.Editor.BaseEditorIaContr
 		var text = this.getFormulaText(mol);
 		var setter = this.getTextSetterWidget(true);
 		setter._applied = false;
+
+		var parentElem = this.getEditor().getCoreElement();
+		setter.appendToElem(parentElem);  // ensure setter widget is a child of parentElem, since popup show may change the parent each time
+
 		var slabel = text || '';
 		//console.log(block, text, slabel);
 		setter.setValue(slabel);
@@ -5179,6 +5183,10 @@ Kekule.Editor.TextBlockIaController = Class.create(Kekule.Editor.ContentBlockIaC
 		var slabel = text || Kekule.$L('ChemWidgetTexts.CAPTION_TEXTBLOCK_INIT'); //Kekule.ChemWidgetTexts.CAPTION_TEXTBLOCK_INIT;
 		//console.log(block, text, slabel);
 		setter.setValue(slabel);
+
+		var parentElem = this.getEditor().getCoreElement();
+		setter.appendToElem(parentElem);  // ensure setter widget is a child of parentElem, since popup show may change the parent each time
+
 		//setter.setValue('hehr');
 		//setter.setIsPopup(true);
 		var style = setter.getElement().style;
