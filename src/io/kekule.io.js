6a3ff5e6e3d75372ac89410cc80a912ffd5bb176
partridgejiang <partridge@cpu.edu.cn>
2018-12-12 20:40:35 +0800

    Avoid a possible exception in reading from local file

--- src/io/kekule.io.js
+++ src/io/kekule.io.js
@@ -1312,8 +1312,11 @@ Kekule.IO.loadFileData = function(file, callback, formatId, options)
 			{
 				var content = reader.result;
 				var chemObj = Kekule.IO.loadFormatData(content, formatInfo.id, options);
-				var info = chemObj.getSrcInfo();
-				info.fileName = fileName;
+				if (chemObj && chemObj.getSrcInfo)
+				{
+					var info = chemObj.getSrcInfo();
+					info.fileName = fileName;
+				}
 				//var success = !!chemObj;
 				var success = (chemObj !== false);
 				callback(chemObj, success);
