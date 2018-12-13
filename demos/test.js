8c9403e98cc5a2074bc9c7ba6608e8ba33b956c9
Aaron Llanos <allanos@inmotionsoftware.com>
2018-12-13 14:35:05 -0600

    one more update for strucutre compare

--- demos/test.js
+++ demos/test.js
@@ -1,5 +1,4 @@
 const { Kekule } = require("kekule")
-// require('./MolHydrogenIaController')
 
 const CORRECT_MOL = `{"id":"m4","coordPos2D":0,"coordPos3D":0,"renderOptions":{"expanded":true,"__type__":"object"},"coord2D":{"x":14.423182679509424,"y":43.103551690568445,"__type__":"object"},"charge":0,"parity":null,"ctab":{"nodes":[{"__type__":"Kekule.Atom","id":"a36","coordPos2D":0,"coordPos3D":0,"overrideRenderOptionItems":[],"coord2D":{"x":0,"y":0,"__type__":"object"},"charge":0,"parity":null,"isotopeId":"C"},{"__type__":"Kekule.Atom","id":"a37","coordPos2D":0,"coordPos3D":0,"overrideRenderOptionItems":[],"coord2D":{"x":0,"y":-0.799982399806396,"__type__":"object"},"charge":0,"parity":null,"isotopeId":"C"},{"__type__":"Kekule.Atom","id":"a33","coordPos2D":0,"coordPos3D":0,"overrideRenderOptionItems":[],"coord2D":{"x":0,"y":0.7999823998063889,"__type__":"object"},"charge":0,"parity":null,"isotopeId":"C"},{"__type__":"Kekule.Atom","id":"a41","coordPos2D":0,"coordPos3D":0,"overrideRenderOptionItems":[],"coord2D":{"x":0,"y":-1.599964799612792,"__type__":"object"},"charge":0,"parity":null,"isotopeId":"C"},{"__type__":"Kekule.Atom","id":"a45","coordPos2D":0,"coordPos3D":0,"renderOptions":{"charDirection":1,"__type__":"object"},"overrideRenderOptionItems":[],"coord2D":{"x":-0.6928050808127786,"y":1.1999735997095868,"__type__":"object"},"charge":0,"parity":null,"isotopeId":"O"}],"anchorNodes":[],"connectors":[{"__type__":"Kekule.Bond","id":"b34","coordPos2D":0,"coordPos3D":0,"overrideRenderOptionItems":[],"parity":null,"bondType":"covalent","bondOrder":1,"electronCount":2,"isInAromaticRing":false,"connectedObjs":[0,1]},{"__type__":"Kekule.Bond","id":"b33","coordPos2D":0,"coordPos3D":0,"overrideRenderOptionItems":[],"parity":0,"bondType":"covalent","bondOrder":2,"electronCount":4,"isInAromaticRing":false,"connectedObjs":[0,2]},{"__type__":"Kekule.Bond","id":"b38","coordPos2D":0,"coordPos3D":0,"overrideRenderOptionItems":[],"parity":null,"bondType":"covalent","bondOrder":1,"electronCount":2,"isInAromaticRing":false,"connectedObjs":[1,3]},{"__type__":"Kekule.Bond","id":"b42","coordPos2D":0,"coordPos3D":0,"overrideRenderOptionItems":[],"parity":null,"bondType":"covalent","bondOrder":1,"electronCount":2,"isInAromaticRing":false,"connectedObjs":[2,4]}],"__type__":"Kekule.StructureConnectionTable"},"__type__":"Kekule.Molecule"}`;
 const INCORRECT_MOL = `{"id":"m1","renderOptions":{"expanded":true,"__type__":"object"},"charge":0,"parity":null,"ctab":{"nodes":[{"__type__":"Kekule.Atom","id":"a2","coord2D":{"x":10.499070323027551,"y":37.998333333333335,"__type__":"object"},"charge":0,"parity":null,"isotopeId":"C"},{"__type__":"Kekule.Atom","id":"a1","coord2D":{"x":9.80625,"y":37.598333333333336,"__type__":"object"},"charge":0,"parity":null,"isotopeId":"C"}],"anchorNodes":[],"connectors":[{"__type__":"Kekule.Bond","id":"b1","parity":null,"bondType":"covalent","bondOrder":1,"electronCount":2,"isInAromaticRing":false,"connectedObjs":[0,1]}],"__type__":"Kekule.StructureConnectionTable"},"__type__":"Kekule.Molecule"}`;
@@ -53,15 +52,52 @@ var composer;
 
 // initialize Composer
 Kekule.X.domReady(function(){
-  const composerElement = document.getElementById('composer')
-  if (!composerElement) {
-    return;
-  }
-  composer = new Kekule.Editor.Composer()
+  var composerEl = document.getElementById('composer');
+  if (composerEl != null) {
+    composer = new Kekule.Editor.Composer(composerEl)
+    setUpComposer(composer)
+    let validKekule = Kekule.IO.loadFormatData(CORRECT_MOL, 'Kekule-JSON');
+    composer.setChemObj(validKekule)
 
+    var skeletalDisplay = document.getElementById('skeletal-switch')
+    skeletalDisplay.addEventListener('change', function changeSkeletalDisplay (event) {
+      let renderConfigs = composer.getRenderConfigs()
+      let newDisplayType = 2 //2 is CONDENSED
+      if(event.target.checked){
+        newDisplayType = 1 //1 is SKELETAL
+      }
+      renderConfigs.getMoleculeDisplayConfigs().setDefMoleculeDisplayType(newDisplayType)
+      composer.getEditor().repaint()
+    })
+  }
+
+  var btnCompare = document.getElementById('btnCompare')
+  if (btnCompare) {
+    var chemComposer0 = document.getElementById('chemComposer0')
+    var chemComposer1 = document.getElementById('chemComposer1')
+    composer = new Kekule.Editor.Composer(chemComposer0)
+    var composer0 = composer;
+    //setUpComposer(composer)
+    composer = new Kekule.Editor.Composer(chemComposer1)
+    var composer1 = composer;
+    //setUpComposer(composer)
+    btnCompare.addEventListener('click', function compareMolecules () {
+      var mol1 = composer0.exportObj(Kekule.StructureFragment);
+      var mol2 = composer1.exportObj(Kekule.StructureFragment);
+      var isSame = mol1 && mol2 && mol1.isSameStructureWith(mol2);
+      var sResult = isSame? 'Same molecules': 'Different molecules';
+      var sClass = isSame? 'Same': 'Diff';
+      var elem = document.getElementById('labelResult');
+      elem.className = sClass;
+      elem.innerHTML = sResult;
+    })
+  }
+})
+
+function setUpComposer(composer) {
   var BNS = Kekule.ChemWidget.ComponentWidgetNames;
   console.log('BNS', BNS)
-  composer.setCommonToolButtons([BNS.saveData, BNS.undo, BNS.redo, BNS.zoomIn, BNS.zoomOut, BNS.objInspector]).setChemToolButtons([
+  composer.setCommonToolButtons([BNS.saveData, BNS.loadData, BNS.undo, BNS.redo, BNS.zoomIn, BNS.reset, BNS.zoomOut, BNS.objInspector]).setChemToolButtons([
     BNS.manipulate,
     BNS.erase,
     {'name': 'Custom', 'actionClass': Kekule.Editor.ActionOnComposerAdv,
@@ -93,19 +129,7 @@ Kekule.X.domReady(function(){
     // BNS.molCharge,
     // BNS.textImage
   ]);
-  let validKekule = Kekule.IO.loadFormatData(CORRECT_MOL, 'Kekule-JSON');
-  composer.setChemObj(validKekule)
-  var skeletalDisplay = document.getElementById('skeletal-switch')
-  skeletalDisplay.addEventListener('change', function changeSkeletalDisplay (event) {
-    let renderConfigs = composer.getRenderConfigs()
-    let newDisplayType = 2 //2 is CONDENSED
-    if(event.target.checked){
-      newDisplayType = 1 //1 is SKELETAL
-    }
-    renderConfigs.getMoleculeDisplayConfigs().setDefMoleculeDisplayType(newDisplayType)
-    composer.getEditor().repaint()
-  })
-})
+}
 
 function validateKekule (kekuleJSON) {
     let validKekule = false;
