aaa8d1b11024016d261e523b715a8e796a3605cd
partridgejiang <partridge@cpu.edu.cn>
2019-02-28 19:49:05 +0800

    Fix a bug of setting non hash global options

--- src/core/kekule.common.js
+++ src/core/kekule.common.js
@@ -196,12 +196,10 @@ Kekule.globalOptions = {
 	add: function(optionName, valueOrHash)
 	{
 		var oldValue = Object.getCascadeFieldValue(optionName, Kekule.globalOptions);
-		if (oldValue)  // value already exists
+
+		if (oldValue && DataType.isObjectValue(oldValue) && DataType.isObjectValue(valueOrHash))  // value already exists and can be extended
 		{
-			if (DataType.isObjectValue(oldValue) && (DataType.isObjectValue(valueOrHash)))
-			{
-				Object.extend(oldValue, valueOrHash);
-			}
+			Object.extend(oldValue, valueOrHash);
 		}
 		else
 			Object.setCascadeFieldValue(optionName, valueOrHash, Kekule.globalOptions, true);
