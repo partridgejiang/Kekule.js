dc2ac7e02f2d11aca3a868c4e455247967ffc9dd
partridgejiang <partridge@cpu.edu.cn>
2019-06-26 21:49:09 +0800

    Create new release file

--- src/core/kekule.root.js
+++ src/core/kekule.root.js
@@ -9,7 +9,7 @@
 var Kekule = {
 	LIBNAME: 'Kekule.js',
 	LIBNAME_CORE: 'Kekule',
-	VERSION: '0.9.0.19062303',
+	VERSION: '0.9.0.19062600',
 	/**
 	 * A flag that indicate whether all essential Kekule modules are loaded into document.
 	 * @ignore
