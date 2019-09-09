b6999900a38bfb269af5f60b1ac6fdf379432be5
partridgejiang <partridge@cpu.edu.cn>
2019-09-09 10:19:57 +0800

    Add observeElemResize property to BaseWidget
    If true, the ResizeObserver will be used to detect the size changing of
    widget element.

--- src/widgets/kekule.widget.base.js
+++ src/widgets/kekule.widget.base.js
@@ -343,6 +343,7 @@ var widgetBindingField = '__$kekule_widget__';
  * @property {Hash} autoResizeConstraints A hash of {width, height}, each value from 0-1 indicating the ratio of widget width/height to client.
  *   If this property is set, widget will automatically adjust its size when the browser window is resized.
  * @property {Bool} autoAdjustSizeOnPopup Whether shrink to browser visible client size when popping up or dropping down.
+ * @property {Bool} observeElemResize Whether use a resize observer to detect the resizing of element and evoke the resized method in supported browser.
  * @property {Bool} isPopup Whether this is a "popup" widget, when click elsewhere on window, the widget will automatically hide itself.
  * @property {Bool} isDialog Whether this is a "dialog" widget, when press ESC key, the widget will automatically hide itself.
  * @property {Kekule.HashEx} iaControllerMap Interaction controller map (id= > controller) linked to this component. Read only.
@@ -1037,6 +1038,28 @@ Kekule.Widget.BaseWidget = Class.create(ObjectEx,
 			}
 		});
 
+		this.defineProp('observeElemResize', {'dataType': DataType.BOOL,
+			'getter': function()
+			{
+				return this.getPropStoreFieldValue('observeElemResize') && Kekule.BrowserFeature.resizeObserver;
+			},
+			'setter': function(value)
+			{
+				if (this.getPropStoreFieldValue('observeElemResize') != (!!value))
+				{
+					this.setPropStoreFieldValue('observeElemResize', !!value);
+					if (value)
+					{
+						this._installElemResizeObserver();
+					}
+					else
+					{
+						this._uninstallElemResizeObserver();
+					}
+				}
+			}
+		});
+
 		this.defineProp('autoAdjustSizeOnPopup', {'dataType': DataType.BOOL, 'scope': Class.PropertyScope.PUBLIC});
 		this.defineProp('isDialog', {'dataType': DataType.BOOL, 'scope': Class.PropertyScope.PUBLIC});
 		this.defineProp('isPopup', {'dataType': DataType.BOOL, 'scope': Class.PropertyScope.PUBLIC});
@@ -1118,6 +1141,13 @@ Kekule.Widget.BaseWidget = Class.create(ObjectEx,
 	/** @private */
 	doFinalize: function($super)
 	{
+		if (this._elemResizeObserver)
+		{
+			if (this._elemResizeObserver.disconnect)
+				this._elemResizeObserver.disconnect();
+			this._elemResizeObserver = null;
+		}
+
 		this.setAction(null);
 		this.setParent(null);
 		this.releaseChildWidgets();
@@ -2199,7 +2229,10 @@ Kekule.Widget.BaseWidget = Class.create(ObjectEx,
 			doResize = true;
 		}
 		if (doResize && (!suppressResize))
-			this.resized();
+		{
+			if (!this.getObserveElemResize())  // if installed elem resize observer, the change of width/height will automatically evoke resized method
+				this.resized();
+		}
 		this.objectChange(['width', 'height']);
 		return this;
 	},
@@ -2251,8 +2284,19 @@ Kekule.Widget.BaseWidget = Class.create(ObjectEx,
 	 */
 	resized: function()
 	{
-		this.doResize();
-		this.invokeEvent('resize', {'widget': this});
+		if (!this._isResizing)
+		{
+			this._isResizing = true;
+			try
+			{
+				this.doResize();
+				this.invokeEvent('resize', {'widget': this});
+			}
+			finally
+			{
+				this._isResizing = false;
+			}
+		}
 	},
 	/**
 	 * Called when width or height of widget changed.
@@ -2262,6 +2306,40 @@ Kekule.Widget.BaseWidget = Class.create(ObjectEx,
 	doResize: function()
 	{
 
+	},
+
+	/** @private */
+	_installElemResizeObserver: function()
+	{
+		if (Kekule.BrowserFeature.resizeObserver)
+		{
+			if (!this._elemResizeObserver)
+			{
+				var self = this;
+				this._elemResizeObserver = new ResizeObserver(function(entries) {
+					for (var i = 0, l = entries.length; i < l; ++i)
+					{
+						var entry = entries[i];
+						if (entry.target && entry.contentBoxSize && entry.target === self.getElement())
+						{
+							self.resized();
+						}
+					}
+				});
+			}
+			this._elemResizeObserver.observe(this.getElement());
+		}
+	},
+	/** @private */
+	_uninstallElemResizeObserver: function()
+	{
+		if (Kekule.BrowserFeature.resizeObserver)
+		{
+			if (this._elemResizeObserver)
+			{
+				this._elemResizeObserver.unobserve(this.getElement());
+			}
+		}
 	},
 
 	/**
