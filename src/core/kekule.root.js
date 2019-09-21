f97f86f9e0839f9d6cac5a9d90b72f384d72f8b3
partridgejiang <partridge@cpu.edu.cn>
2019-09-21 13:18:23 +0800

    Create new dist files

--- src/core/kekule.root.js
+++ src/core/kekule.root.js
@@ -9,7 +9,7 @@
 var Kekule = {
 	LIBNAME: 'Kekule.js',
 	LIBNAME_CORE: 'Kekule',
-	VERSION: '0.9.3.19091401',
+	VERSION: '0.9.3.19092100',
 	/**
 	 * A flag that indicate whether all essential Kekule modules are loaded into document.
 	 * @ignore
