35b002f832d2d8946c19826733ed609f54128163
partridgejiang <partridge@cpu.edu.cn>
2021-02-14 14:26:50 +0800

    Add Kekule.ValenceUtils.getPossibleMdlValenceInfo function

--- src/core/kekule.valences.js
+++ src/core/kekule.valences.js
@@ -1,6 +1,6 @@
 /**
  * @fileoverview
- * This file helps to determine the valence of a neutral, anion(-) or cation(+) atom. 
+ * This file helps to determine the valence of a neutral, anion(-) or cation(+) atom.
  * @author Partridge Jiang
  */
 
@@ -9,6 +9,373 @@
  * @class
  */
 Kekule.ValenceUtils = {
+	/**
+	 * Returns possible valence according to atomic number and charge.
+	 * @param {Int} atomicNumber
+	 * @param {Int} charge
+	 * @returns {Hash} A special hash containing fields {valences: array, valenceElectronCount: int(in no charge form), unexpectedCharge: a special flag, indicating that the input charge is not a normal one}
+	 */
+	getPossibleMdlValenceInfo: function(atomicNumber, charge)
+	{
+		var q = Math.round(charge);
+		var valences = [];
+		var valenceElectronCount = 0;
+		var unexpectedCharge = false;
+		var result = {};
+
+		switch (atomicNumber) {
+		  case 1: // H
+		  case 3: // Li
+		  case 11: // Na
+		  case 19: // K
+		  case 37: // Rb
+		  case 55: // Cs
+		  case 87: // Fr
+		    if (q === 0)
+			    valences = [1];
+		    else if (q === 1)
+			    valences = [0];
+		    else
+			    unexpectedCharge = true;
+			  valenceElectronCount = 1;
+		    break;
+
+		  case 4: // Be
+		  case 12: // Mg
+		  case 20: // Ca
+		  case 38: // Sr
+		  case 56: // Ba
+		  case 88: // Ra
+		    switch (q) {
+			    case 0: valences = [2]; break;
+			    case 1: valences = [1]; break;
+			    case 2: valences = [0]; break;
+			    default: unexpectedCharge = true; break;
+		    }
+			  valenceElectronCount = 2;
+		    break;
+
+		  case 5: // B
+		    switch (q) {
+			    case -4: valences = [1]; break;
+			    case -3: valences = [2]; break;
+			    case -2: valences = [3, 5]; break;
+			    case -1: valences = [4]; break;
+			    case 0: valences = [3]; break;
+			    case 1: valences = [2]; break;
+			    case 2: valences = [1]; break;
+			    case 3: valences = [0]; break;
+			    default: unexpectedCharge = true; break;
+		    }
+			  valenceElectronCount = 3;
+		    break;
+
+		  case 6: // C
+		    switch (q) {
+			    case -4: valences = [0]; break;
+			    case -3: valences = [1]; break;
+			    case -2: valences = [2]; break;
+			    case -1: valences = [3, 5]; break;
+			    case 0: valences = [4]; break;
+			    case 1: valences = [3]; break;
+			    case 2: valences = [2]; break;
+			    case 3: valences = [1]; break;
+			    case 4: valences = [0]; break;
+			    default: unexpectedCharge = true; break;
+		    }
+			  valenceElectronCount = 4;
+		    break;
+
+		  case 7: // N
+		    switch (q) {
+			    case -3: valences = [0]; break;
+			    case -2: valences = [1]; break;
+			    case -1: valences = [2]; break;
+			    case 0: valences = [3, 5]; break;
+			    case 1: valences = [4]; break;
+			    case 2: valences = [3]; break;
+			    case 3: valences = [2]; break;
+			    case 4: valences = [1]; break;
+			    case 5: valences = [0]; break;
+			    default: unexpectedCharge = true; break;
+		    }
+			  valenceElectronCount = 5;
+		    break;
+
+		  case 8: // O
+		    switch (q) {
+			    case -2: valences = [0]; break;
+			    case -1: valences = [1]; break;
+			    case 0: valences = [2]; break;
+			    case 1: valences = [3, 5]; break;
+			    case 2: valences = [4]; break;
+			    case 3: valences = [3]; break;
+			    case 4: valences = [2]; break;
+			    case 5: valences = [1]; break;
+			    case 6: valences = [0]; break;
+			    default: unexpectedCharge = true; break;
+		    }
+			  valenceElectronCount = 6;
+		    break;
+
+		  case 9: // F
+		    switch (q) {
+			    case -1: valences = [0]; break;
+			    case 0: valences = [1]; break;
+			    case 1: valences = [2]; break;
+			    case 2: valences = [3, 5]; break;
+			    case 3: valences = [4]; break;
+			    case 4: valences = [3]; break;
+			    case 5: valences = [2]; break;
+			    case 6: valences = [1]; break;
+			    case 7: valences = [0]; break;
+			    default: unexpectedCharge = true; break;
+		    }
+			  valenceElectronCount = 7;
+		    break;
+
+		  case 13: // Al
+		    switch (q) {
+			    case -4: valences = [1, 3, 5, 7]; break;
+			    case -3: valences = [2, 4, 6]; break;
+			    case -2: valences = [3, 5]; break;
+			    case -1: valences = [4]; break;
+			    case 0: valences = [3]; break;
+			    case 1: valences = [2]; break;
+			    case 2: valences = [1]; break;
+			    case 3: valences = [0]; break;
+			    default: unexpectedCharge = true; break;
+		    }
+			  valenceElectronCount = 3;
+		    break;
+
+		  case 14: // Si
+		    switch (q) {
+			    case -4: valences = [0]; break;
+			    case -3: valences = [1, 3, 5, 7]; break;
+			    case -2: valences = [2, 4, 6]; break;
+			    case -1: valences = [3, 5]; break;
+			    case 0: valences = [4]; break;
+			    case 1: valences = [3]; break;
+			    case 2: valences = [2]; break;
+			    case 3: valences = [1]; break;
+			    case 4: valences = [0]; break;
+		    }
+			  valenceElectronCount = 4;
+		    break;
+
+		  case 15: // P
+		    switch (q) {
+			    case -3: valences = [0]; break;
+			    case -2: valences = [1, 3, 5, 7]; break;
+			    case -1: valences = [2, 4, 6]; break;
+			    case 0: valences = [3, 5]; break;
+			    case 1: valences = [4]; break;
+			    case 2: valences = [3]; break;
+			    case 3: valences = [2]; break;
+			    case 4: valences = [1]; break;
+			    case 5: valences = [0]; break;
+		    }
+			  valenceElectronCount = 5;
+		    break;
+
+		  case 16: // S
+		    switch (q) {
+			    case -2: valences = [0]; break;
+			    case -1: valences = [1, 3, 5, 7]; break;
+			    case 0: valences = [2, 4, 6]; break;
+			    case 1: valences = [3, 5]; break;
+			    case 2: valences = [4]; break;
+			    case 3: valences = [3]; break;
+			    case 4: valences = [2]; break;
+			    case 5: valences = [1]; break;
+			    case 6: valences = [0]; break;
+		    }
+			  valenceElectronCount = 6;
+		    break;
+
+		  case 17: // Cl
+		    switch (q) {
+			    case -1: valences = [0]; break;
+			    case 0: valences = [1, 3, 5, 7]; break;
+			    case 1: valences = [2, 4, 6]; break;
+			    case 2: valences = [3, 5]; break;
+			    case 3: valences = [4]; break;
+			    case 4: valences = [3]; break;
+			    case 5: valences = [2]; break;
+			    case 6: valences = [1]; break;
+			    case 7: valences = [0]; break;
+		    }
+			  valenceElectronCount = 7;
+		    break;
+
+			case 31: // Ga
+				switch (q) {
+					case -4: valences = [1, 3, 5, 7]; break;
+					case -3: valences = [2, 4, 6]; break;
+					case -2: valences = [3, 5]; break;
+					case -1: valences = [4]; break;
+					case 0: valences = [3]; break;
+					case 2: valences = [1]; break;
+					case 3: valences = [0]; break;
+					default: unexpectedCharge = true; break;
+				}
+				valenceElectronCount = 3;
+				break;
+
+		  case 32: // Ge
+		    switch (q) {
+			    case -4: valences = [0]; break;
+			    case -3: valences = [1, 3, 5, 7]; break;
+			    case -2: valences = [2, 4, 6]; break;
+			    case -1: valences = [3, 5]; break;
+			    case 0: valences = [4]; break;
+			    case 1: valences = [3]; break;
+			    case 3: valences = [1]; break;
+			    case 4: valences = [0]; break;
+		    }
+			  valenceElectronCount = 4;
+		    break;
+
+		  case 33: // As
+		    switch (q) {
+			    case -3: valences = [0]; break;
+			    case -2: valences = [1, 3, 5, 7]; break;
+			    case -1: valences = [2, 4, 6]; break;
+			    case 0: valences = [3, 5]; break;
+			    case 1: valences = [4]; break;
+			    case 2: valences = [3]; break;
+			    case 4: valences = [1]; break;
+			    case 5: valences = [0]; break;
+		    }
+			  valenceElectronCount = 5;
+		    break;
+
+			case 34: // Se
+		    switch (q) {
+			    case -2: valences = [0]; break;
+			    case -1: valences = [1, 3, 5, 7]; break;
+			    case 0: valences = [2, 4, 6]; break;
+			    case 1: valences = [3, 5]; break;
+			    case 2: valences = [4]; break;
+			    case 3: valences = [3]; break;
+			    case 5: valences = [1]; break;
+			    case 6: valences = [0]; break;
+		    }
+			  valenceElectronCount = 6;
+		    break;
+
+			case 35: // Br
+		    switch (q) {
+			    case -1: valences = [0]; break;
+			    case 0: valences = [1, 3, 5, 7]; break;
+			    case 1: valences = [2, 4, 6]; break;
+			    case 2: valences = [3, 5]; break;
+			    case 3: valences = [4]; break;
+			    case 4: valences = [3]; break;
+			    case 6: valences = [1]; break;
+			    case 7: valences = [0]; break;
+		    }
+			  valenceElectronCount = 7;
+		    break;
+
+		  case 49: // In
+				switch (q) {
+					case -4: valences = [1, 3, 5, 7]; break;
+					case -3: valences = [2, 4, 6]; break;
+					case -2: valences = [3, 5]; break;
+					case -1: valences = [4]; break;
+					case 0: valences = [3]; break;
+					case 2: valences = [1]; break;
+					case 3: valences = [0]; break;
+					default: unexpectedCharge = true; break;
+				}
+				valenceElectronCount = 3;
+				break;
+
+			case 50: // Sn
+			case 82: // Pb
+		    switch (q) {
+			    case -4: valences = [0]; break;
+			    case -3: valences = [1, 3, 5, 7]; break;
+			    case -2: valences = [2, 4, 6]; break;
+			    case -1: valences = [3, 5]; break;
+			    case 0: valences = [2, 4]; break;
+			    case 1: valences = [3]; break;
+			    case 3: valences = [1]; break;
+			    case 4: valences = [0]; break;
+		    }
+			  valenceElectronCount = 4;
+		    break;
+
+			case 51: // Sb
+			case 83: // Bi
+		    switch (q) {
+			    case -3: valences = [0]; break;
+			    case -2: valences = [1, 3, 5, 7]; break;
+			    case -1: valences = [2, 4, 6]; break;
+			    case 0: valences = [3, 5]; break;
+			    case 1: valences = [2, 4]; break;
+			    case 2: valences = [3]; break;
+			    case 4: valences = [1]; break;
+			    case 5: valences = [0]; break;
+		    }
+			  valenceElectronCount = 5;
+		    break;
+
+			case 52: // Te
+			case 84: // Po
+		    switch (q) {
+			    case -2: valences = [0]; break;
+			    case -1: valences = [1, 3, 5, 7]; break;
+			    case 0: valences = [2, 4, 6]; break;
+			    case 1: valences = [3, 5]; break;
+			    case 2: valences = [2, 4]; break;
+			    case 3: valences = [3]; break;
+			    case 5: valences = [1]; break;
+			    case 6: valences = [0]; break;
+		    }
+			  valenceElectronCount = 6;
+		    break;
+
+			case 53: // I
+			case 85: // At
+		    switch (q) {
+			    case -1: valences = [0]; break;
+			    case 0: valences = [1, 3, 5, 7]; break;
+			    case 1: valences = [2, 4, 6]; break;
+			    case 2: valences = [3, 5]; break;
+			    case 3: valences = [2, 4]; break;
+			    case 4: valences = [3]; break;
+			    case 6: valences = [1]; break;
+			    case 7: valences = [0]; break;
+		    }
+			  valenceElectronCount = 7;
+		    break;
+
+			case 81: // Tl
+				switch (q) {
+					case -4: valences = [1, 3, 5, 7]; break;
+					case -3: valences = [2, 4, 6]; break;
+					case -2: valences = [3, 5]; break;
+					case -1: valences = [2, 4]; break;
+					case 0: valences = [1, 3]; break;
+					case 3: valences = [0]; break;
+					default: unexpectedCharge = true; break;
+				}
+				valenceElectronCount = 3;
+				break;
+		  }
+
+		  if (unexpectedCharge)  // calculate the valence direct from outer shell valence electron count
+		  {
+		  	var eCount = valenceElectronCount - charge;
+		  	if (eCount < 0 || eCount > 8)  // related to the inner shell, complex and we do not handle here
+				  valences = [];
+		  }
+
+		  return {'valences': valences, 'valenceElectronCount': valenceElectronCount, 'unexpectedCharge': unexpectedCharge};
+	},
 	/**
 	 * Returns the implicit MDL valence for element with charge and known (explicit) valence.
 	 * e.g. C+ valence is 3 while C- valence is also 3.
@@ -23,7 +390,7 @@ Kekule.ValenceUtils = {
 	{
 		var q = charge;
 		var val = explicitValence;
-		
+
 		switch (atomicNumber) {
 		  case 1: // H
 		  case 3: // Li
@@ -35,7 +402,7 @@ Kekule.ValenceUtils = {
 		    if (q == 0 && val <= 1)
 		      return 1;
 		    break;
-		
+
 		  case 4: // Be
 		  case 12: // Mg
 		  case 20: // Ca
@@ -47,7 +414,7 @@ Kekule.ValenceUtils = {
 		    case 1: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 5: // B
 		    switch (q) {
 		    case -4: if (val <= 1) return 1; break;
@@ -60,7 +427,7 @@ Kekule.ValenceUtils = {
 		    case 2: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 6: // C
 		    switch (q) {
 		    case -3: if (val <= 1) return 1; break;
@@ -73,7 +440,7 @@ Kekule.ValenceUtils = {
 		    case 3: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 7: // N
 		    switch (q) {
 		    case -2: if (val <= 1) return 1; break;
@@ -86,7 +453,7 @@ Kekule.ValenceUtils = {
 		    case 4: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 8: // O
 		    switch (q) {
 		    case -1: if (val <= 1) return 1; break;
@@ -99,7 +466,7 @@ Kekule.ValenceUtils = {
 		    case 5: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 9: // F
 		    switch (q) {
 		    case 0: if (val <= 1) return 1; break;
@@ -112,7 +479,7 @@ Kekule.ValenceUtils = {
 		    case 6: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 13: // Al
 		    switch (q) {
 		    case -4: if (val <= 1) return 1;
@@ -130,7 +497,7 @@ Kekule.ValenceUtils = {
 		    case 2: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 14: // Si
 		    switch (q) {
 		    case -3: if (val <= 1) return 1;
@@ -148,7 +515,7 @@ Kekule.ValenceUtils = {
 		    case 3: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 15: // P
 		    switch (q) {
 		    case -2: if (val <= 1) return 1;
@@ -166,7 +533,7 @@ Kekule.ValenceUtils = {
 		    case 4: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 16: // S
 		    switch (q) {
 		    case -1: if (val <= 1) return 1;
@@ -184,7 +551,7 @@ Kekule.ValenceUtils = {
 		    case 5: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 17: // Cl
 		    switch (q) {
 		    case 0: if (val <= 1) return 1;
@@ -202,7 +569,7 @@ Kekule.ValenceUtils = {
 		    case 6: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 31: // Ga
 		    switch (q) {
 		    case -4: if (val <= 1) return 1;
@@ -219,7 +586,7 @@ Kekule.ValenceUtils = {
 		    case 2: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 32: // Ge
 		    switch (q) {
 		    case -3: if (val <= 1) return 1;
@@ -236,7 +603,7 @@ Kekule.ValenceUtils = {
 		    case 3: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 33: // As
 		    switch (q) {
 		    case -2: if (val <= 1) return 1;
@@ -253,7 +620,7 @@ Kekule.ValenceUtils = {
 		    case 4: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 34: // Se
 		    switch (q) {
 		    case -1: if (val <= 1) return 1;
@@ -270,7 +637,7 @@ Kekule.ValenceUtils = {
 		    case 5: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 35: // Br
 		    switch (q) {
 		    case 0: if (val <= 1) return 1;
@@ -287,7 +654,7 @@ Kekule.ValenceUtils = {
 		    case 6: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 49: // In
 		    switch (q) {
 		    case -4: if (val <= 1) return 1;
@@ -305,7 +672,7 @@ Kekule.ValenceUtils = {
 		    case 2: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 50: // Sn
 		  case 82: // Pb
 		    switch (q) {
@@ -324,7 +691,7 @@ Kekule.ValenceUtils = {
 		    case 3: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 51: // Sb
 		  case 83: // Bi
 		    switch (q) {
@@ -343,7 +710,7 @@ Kekule.ValenceUtils = {
 		    case 4: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 52: // Te
 		  case 84: // Po
 		    switch (q) {
@@ -362,7 +729,7 @@ Kekule.ValenceUtils = {
 		    case 5: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 53: // I
 		  case 85: // At
 		    switch (q) {
@@ -381,7 +748,7 @@ Kekule.ValenceUtils = {
 		    case 6: if (val <= 1) return 1; break;
 		    }
 		    break;
-		
+
 		  case 81: // Tl
 		    switch (q) {
 		    case -4: if (val <= 1) return 1;
@@ -399,11 +766,11 @@ Kekule.ValenceUtils = {
 		              if (val <= 3) return 3; break;
 		    }
 		    break;
-		
+
 		  }
 		  return val;
 	},
-	
+
 	/**
 	 * Returns the implicit HYD valence for element with charge and known (explicit) valence.
 	 * The code is copy from mdlvalence.h of OpenBabel.
@@ -418,7 +785,7 @@ Kekule.ValenceUtils = {
 		var elem = atomicNumber;
 		var q = charge;
 		var val = explicitValence;
-		
+
 	  var impval = 0;
 	  if (elem == 6) { // C
 	    impval = 4 - Math.abs(q);
