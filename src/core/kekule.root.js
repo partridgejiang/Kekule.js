b4b8e6e18886e3d4acdf35b85236f2a1918ccac1
partridgejiang <partridge@cpu.edu.cn>
2021-08-12 15:25:17 +0800

    Update version number

--- src/core/kekule.root.js
+++ src/core/kekule.root.js
@@ -9,7 +9,7 @@
 var Kekule = {
 	LIBNAME: 'Kekule.js',
 	LIBNAME_CORE: 'Kekule',
-	VERSION: '0.9.7.21081200',
+	VERSION: '0.9.7.21081201',
 	/**
 	 * A flag that indicate whether all essential Kekule modules are loaded into document.
 	 * @ignore
