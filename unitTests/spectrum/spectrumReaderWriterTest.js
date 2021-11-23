a4a3d8f649288f2d8febbcc3aa159269de7279d0
partridgejiang <partridge@cpu.edu.cn>
2021-11-23 15:06:21 +0800

    Add test of the peak assignment output of JCAMP-DX

--- unitTests/spectrum/spectrumReaderWriterTest.js
+++ unitTests/spectrum/spectrumReaderWriterTest.js
@@ -36,7 +36,7 @@ describe('Test of the I/O functions of spectrum', function() {
 		return true;
 	};
 
-	var outputFormats = [/*'cml',*/ 'jcamp-dx'];
+	var outputFormats = ['cml',  'jcamp-dx'];
 	var srcTestFiles = [
 
 		'cmlSpect/example0.cml',
@@ -56,6 +56,8 @@ describe('Test of the I/O functions of spectrum', function() {
 		//'jcamp/BRUKDIF.DX'
 	];
 
+	//srcTestFiles = ['cmlSpect/example1.cml'];
+
 
 	srcTestFiles.forEach(function(srcFile){
 		it('Test reader/writer based on src ' + srcFile, function(done) {
@@ -71,15 +73,21 @@ describe('Test of the I/O functions of spectrum', function() {
 					var chemObj2 = Kekule.IO.loadFormatData(outputData, format);
 					var spectrum2 = getSpectrumInside(chemObj2);
 					expect(spectrum2 instanceof Kekule.Spectroscopy.Spectrum).toEqual(true);
+					//console.log(spectrum2, spectrum2.getRefMolecules());
+					//console.log(spectrum, spectrum.getRefMolecules());
+					//console.log(outputData);
 					//spectrum2.getActiveDataSection().setRawValueAt(1, [0, 0]);
 					//expect(isSpectrumEqual(spectrum, spectrum2)).toEqual(true);
 					var spectrum1 = spectrum;
 					if (srcFile.endsWith('.cml') && format === 'jcamp-dx')
 					{
-						spectrum1 = spectrum.clone();
+						//spectrum1 = spectrum.clone();
+						spectrum1 = getSpectrumInside(chemObj.clone());  // clone and restain the possible refMolecules
+						/*
 						// CML file may containing refMolecules that can not be handled by JCAMP now, remove it
 						spectrum1.setRefMolecules(undefined);
 						spectrum2.setRefMolecules(undefined);
+						*/
 						// CML file may containing peak details that can not be handled by JCAMP now, remove them
 						for (var i = 0, l = spectrum1.getDataSectionCount(); i < l; ++i)
 						{
@@ -88,13 +96,28 @@ describe('Test of the I/O functions of spectrum', function() {
 							{
 								for (var j = 0, jj = section.getDataCount(); j < jj; ++j)
 								{
-									section.setExtraInfoAt(j, undefined);
+									//section.setExtraInfoAt(j, undefined);
+									var extra = section.getExtraInfoAt(j);
+									if (extra instanceof Kekule.Spectroscopy.SpectrumPeakDetails)
+									{
+										if (extra.getAssignment())
+										{
+											extra.setShape(undefined);
+											extra.setMultiplicity(undefined);
+										}
+										else
+										{
+											section.setExtraInfoAt(j, undefined);
+										}
+									}
 								}
 							}
 						}
-						// the meta may be different in CML/JCAMP
-						compareOptions.ignoredProperties = ['metaData', 'conditions', 'parameters'];
 					}
+					// the meta may be different in CML/JCAMP
+					if ((srcFile.endsWith('.cml') && format === 'jcamp-dx')
+						|| (!srcFile.endsWith('.cml') && format === 'cml'))
+						compareOptions.ignoredProperties = ['metaData', 'conditions', 'parameters'];
 
 					expect(spectrum1.equal(spectrum2, compareOptions)).toEqual(true);
 					spectrum2.finalize();
