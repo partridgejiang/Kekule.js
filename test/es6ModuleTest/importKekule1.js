6a060a3342285798e3331218e90e7a58cb1faf21
partridgejiang <partridge@cpu.edu.cn>
2022-12-21 20:22:26 +0800

    New module file import tests

---
+++ test/es6ModuleTest/importKekule1.js
@@ -0,0 +1,7 @@
+import exporter from "../../dist/kekule.moduleEnvInits.esm.mjs";
+import "../../dist/kekule.js";
+import "../../dist/dividedMin/root.min.js";
+import "../../dist/dividedMin/localization.min.js";
+import "../../dist/dividedMin/common.min.js";
+export default exporter();
+
---
+++ test/es6ModuleTest/importKekule2.js
@@ -0,0 +1,8 @@
+import exporter from "../../dist/kekule.moduleEnvInits.esm.mjs";
+import "../../dist/kekule.js";
+import "../../dist/dividedMin/root.min.js";
+import "../../dist/dividedMin/localization.min.js";
+import "../../dist/dividedMin/common.min.js";
+import "../../dist/dividedMin/core.min.js";
+import "../../dist/dividedMin/io.min.js";
+export default exporter();
