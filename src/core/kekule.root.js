bb17ea6a7ee2de437943c0b4eaac66fe8acf1ea4
partridgejiang <partridge@cpu.edu.cn>
2024-12-10 22:51:33 +0800

    Create new dist files

--- src/core/kekule.root.js
+++ src/core/kekule.root.js
@@ -11,7 +11,7 @@ var Kekule = (typeof(Kekule) === 'object')? Kekule: {};
 var Kekule = Object.extend(Kekule, {
 	LIBNAME: 'Kekule.js',
 	LIBNAME_CORE: 'Kekule',
-	VERSION: '1.0.1.24051100',
+	VERSION: '1.0.1.24121000',
 	/**
 	 * A flag that indicate whether all essential Kekule modules are loaded into document.
 	 * @ignore
