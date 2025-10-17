5eb2ff895eb2de7d9736d1f184dfbbfcc5a6f838
partridgejiang <partridge@cpu.edu.cn>
2025-10-17 19:06:11 +0800

    Update new version number

--- src/core/kekule.root.js
+++ src/core/kekule.root.js
@@ -11,7 +11,7 @@ var Kekule = (typeof(Kekule) === 'object')? Kekule: {};
 var Kekule = Object.extend(Kekule, {
 	LIBNAME: 'Kekule.js',
 	LIBNAME_CORE: 'Kekule',
-	VERSION: '1.0.3.25070500',
+	VERSION: '1.0.3.25101700',
 	/**
 	 * A flag that indicate whether all essential Kekule modules are loaded into document.
 	 * @ignore
